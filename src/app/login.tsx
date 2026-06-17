cb3d3da0a83fa36dcf079f705b36a5859b5732f4
Jerry Leonardo <136923571+jeh7sx@users.noreply.github.com>
2026-06-17 07:18:55 -0300

    Atualizar o login.tsx

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -71,7 +71,7 @@ export default function Login() {
 
       Alert.alert('Sucesso', 'Login realizado.');
 
-      router.replace('/visaoGeral');
+      router.replace('/home');
 
       await registrarLog(
         auth.currentUser?.uid || '',
